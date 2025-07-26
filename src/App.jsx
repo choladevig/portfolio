f22e06f20c918678855740ceb23829036fe3d876
choladevig <choladevi.gheereddy@gmail.com>
2025-07-26 16:03:34 -0400

    Portfolio submitted

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Education from './components/Education';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Slide from './components/slide';
+import Slide from './components/Slide';
 import Certifications from './components/Certifications';
 
 export default function App() {
